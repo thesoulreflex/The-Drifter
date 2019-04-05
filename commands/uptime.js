b56ada6b18e64aa2b046ab20d657b196c995d2e3
thesoulreflex <48872834+thesoulreflex@users.noreply.github.com>
2019-04-04 21:50:50 -0500

    Uptime

---
+++ commands/uptime.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const config = require("../../config.json");
+let prefix = config.prefix;
+
+module.exports.run = async (bot, message, args) => {
+    function duration(ms){
+        const sec = Math.floor((ms / 1000) % 60).toString();
+        const min = Math.floor((ms / (1000 * 60)) % 60).toString();
+        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString();
+        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString();
+
+        return `${days.padStart(1, '0')}d, ${hrs.padStart(2, '0')}h, ${min.padStart(2, '0')}m, ${sec.padStart(2, '0')}s`
+    }
+
+    message.channel.send(`I have served you for... \n\`${duration(bot.uptime)}\``);
+}
+
+module.exports.help = {
+    name: "uptime"
+}
