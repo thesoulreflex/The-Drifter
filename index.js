811f5e2e08798e14f303f0d87697efd442a22da8
thesoulreflex <48872834+thesoulreflex@users.noreply.github.com>
2019-05-01 17:03:32 -0500

    cpmmand

--- index.js
+++ index.js
@@ -1,54 +1,154 @@
+const Discord = require("discord.js");
 const config = require("./config.json");
-const discord = require("discord.js");
+const bot = new Discord.Client();
 const fs = require("fs");
-const bot = new discord.Client()
-const token = process.env.token;
 let prefix = config.prefix;
-bot.commands = new discord.Collection();
-
-fs.readdir("./commands/", (err, files)=> {
-
-    if(err) console.log(err);
-    
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <=0){
-
-        console.log("Couldn't find commands!");
-        return;
-    }
-
-    jsfile.forEach((f, i) => {
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-
-
-    });
-})
+bot.commands = new Discord.Collection();
 
 
 
-bot.on("ready", async () =>{
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
- bot.user.setPresence({ game: { name: "against Shin Malpur" , type: "PLAYING" }, status: "online" });
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
 });
 
- 
+
+
+fs.readdir("./commands/fun/", (err, files) => {
+
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+  
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/fun/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+
+
+fs.readdir("./commands/general/", (err, files) => {
+  
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+  
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/general/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+
+
+fs.readdir("./commands/moderation/", (err, files) => {
+  
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/moderation/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+
+
+fs.readdir("./commands/nsfw/", (err, files) => {
+  
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/nsfw/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+
+
+fs.readdir("./commands/other/", (err, files) => {
+
+  if(err) console.log(err);
+  
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/other/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+
+
+bot.on("ready", () => {
+  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  bot.user.setPresence({ game: { name: `on ${bot.guilds.size} servers! | ${prefix}help` }, status: 'online' });
+});
+
+
 
 bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    if(!message.content.startsWith(prefix)) return;
+  //a little bit of data parsing/general checks
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  if(!message.content.startsWith(prefix)) return;
+  let content = message.content.split(" ");
+  let command = content[0].toLowerCase();
+  let args = content.slice(1);
 
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(commandfile) commandfile.run(bot,message,args);
-   
-    
-})
 
-    
-bot.login(token);
+  //checks if message contains a command and runs it
+  let commandfile = bot.commands.get(command.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+});
+
+
+
+bot.login(process.env.token);
