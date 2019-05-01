43f5d44a493c47bcd2b5900edd2f58e83daf8ddc
thesoulreflex <48872834+thesoulreflex@users.noreply.github.com>
2019-05-01 15:40:37 -0500

    updated kick command

--- commands/kick.js
+++ commands/kick.js
@@ -1,29 +1,24 @@
-const discord = require("discord.js");
+const Discord = require("discord.js");
 const config = require("../config.json");
+let prefix = config.prefix
 
 module.exports.run = async (bot, message, args) => {
-    
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!kUser) return message.channel.send("Can't find user");
-        let kReason = args.join(" ").slice(22)
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You can not kick this user bitch");
-        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("you cannot kick him you pesant");
-    
-        let kickembed = new discord.RichEmbed()
-        .setDescription("~kick~")
-        .setColor("#faf0be")
-        .addField("kicked User" , `${kUser} with ID ${kUser.id}`)
-        .addField("Kicked By" , `<@${message.author.id}>`)
-        .addField("kicked In" , message.channel)
-        .addField("Time" , message.createdAt)
-        .addField("Reason", kReason);
-    
-        let kickchannel = message.guild.channels.find(`name`, "logs");
-        if(!kickchannel) return message.channel.send("Can not find logs bitch.");
-    
-        message.guild.member(kUser).kick(kReason)
-        kickchannel.send(kickembed);
+    await message.delete();
+    const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+    let kicked = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kicked) return message.reply(`Incorrect usage: \`${prefix}kick @user reason\``);
+    if(kicked.id === message.author.id) return message.reply("Incorrect argument: `user is not kickable`");
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Insufficient Permissions: `KICK_MEMBERS`");
+    let kickreason = args.join(" ").slice(22);
+    if(!kickreason) return message.reply(`Incorrect usage: \`${prefix}kick @user reason\``);
+    if(kicked.hasPermission("ADMINISTRATOR")) return message.reply("Incorrect argument: `user is not kickable`");
+    if(!kicked.kickable()) return message.reply("Incorrect argument: `user is not kickable`");
 
+    kicked.send(`You have been kicked from **${message.channel.guild.name}** for the reason: **${kickreason}**`)
+    await delay(500);
+    message.reply("User kicked!");
+    message.guild.member(kicked).kick(kickreason);
+    return;
 }
 
 module.exports.help = {
