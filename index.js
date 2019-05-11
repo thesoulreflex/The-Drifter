4140d1733b04cf3cda75a4fb03bc6473aac8dc42
thesoulreflex <48872834+thesoulreflex@users.noreply.github.com>
2019-05-10 19:57:04 -0500

    push update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ fs.readdir("./commands/", (err, files)=> {
         return;
     }
 
-    jsfile.forEach((f, i) => {
+    jsfile.forEach((f,i) => {
         let props = require(`./commands/${f}`);
         console.log(`${f} loaded!`);
         bot.commands.set(props.help.name, props);
