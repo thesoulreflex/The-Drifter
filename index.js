2606e46a63f67cd3fb697403d8b3f53387584978
thesoulreflex <48872834+thesoulreflex@users.noreply.github.com>
2019-07-01 17:46:09 -0500

    command fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ fs.readdir("./commands/", (err, files)=> {
     jsfile.forEach((f,i) => {
         let props = require(`./commands/${f}`);
         console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
+        bot.commands.set(props.help.name. props);
 
 
     });
